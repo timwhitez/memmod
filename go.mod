6b21a379342996641cee20b9f1ea3662dae81b49
TimWhite <36320909+timwhitez@users.noreply.github.com>
2022-06-30 16:22:23 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/timwhitez/memmod
+
+go 1.17
+
+require (
+	github.com/timwhitez/Doge-Gabh v1.9.1
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b
+)
+
+require (
+	github.com/Binject/debug v0.0.0-20211007083345-9605c99179ee // indirect
+	github.com/awgh/rawreader v0.0.0-20200626064944-56820a9c6da4 // indirect
+)
---
+++ memmod_windows_64.go
@@ -0,0 +1,40 @@
+//go:build (windows && amd64) || (windows && arm64)
+// +build windows,amd64 windows,arm64
+
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2017-2021 WireGuard LLC. All Rights Reserved.
+ */
+
+package memmod
+
+import (
+	"fmt"
+
+	gabh "github.com/timwhitez/Doge-Gabh/pkg/Gabh"
+	"golang.org/x/sys/windows"
+)
+
+func (opthdr *IMAGE_OPTIONAL_HEADER) imageOffset() uintptr {
+	return uintptr(opthdr.ImageBase & 0xffffffff00000000)
+}
+
+func (module *Module) check4GBBoundaries(alignedImageSize uintptr) (err error) {
+	for (module.codeBase >> 32) < ((module.codeBase + alignedImageSize) >> 32) {
+		node := &addressList{
+			next:    module.blockedMemory,
+			address: module.codeBase,
+		}
+		module.blockedMemory = node
+
+		module.codeBase, err = gabh.NvA(0,
+			alignedImageSize,
+			windows.MEM_RESERVE|windows.MEM_COMMIT,
+			windows.PAGE_READWRITE)
+
+		if err != nil {
+			return fmt.Errorf("Error allocating memory block: %w", err)
+		}
+	}
+	return
+}
